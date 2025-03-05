21b389268436c69f056ed37d8035a6c1345b6433
sunsword2020 <73904317+sunsword2020@users.noreply.github.com>
2025-03-05 21:39:10 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -5,4 +5,4 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("CodeXBotz")
+    return web.json_response("Due to Koyeb's free hobby plan restrictions, the bot will sleep if inactive for an hour. The only way to wake it up is by visiting this link. Sorry for the inconvenience caused")
